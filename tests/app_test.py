1966e5bb24db2bfe5cf1b078dd2aa01729060db7
wangilamain <wangilayng@gmail.com>
2020-09-21 12:36:02 -0400

    added an image link

---
+++ tests/app_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+class PitchTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(username='victor', email='victor@mail.com', password='parseword')
+        self.new_post = Post()
+        self.new_comment = Comment()
+
+    def test_user_instance(self):
+        pass
+
+    def test_post_instance(self):
+        pass
+
+    def test_comment_instance(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
