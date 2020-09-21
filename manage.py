2bb7587db06473ec68191dbb0be727dff8854239
wangilamain <wangilayng@gmail.com>
2020-09-21 12:57:34 -0400

    heroku deployment

---
+++ manage.py
@@ -0,0 +1,25 @@
+from app import create_app,db
+from flask_script import Manager,Server
+from flask_migrate import Migrate,MigrateCommand
+from app.models import Comments,Pitch,Users
+
+app = create_app('development')
+migrate = Migrate(app,db)
+
+manager = Manager(app)
+manager.add_command('server',Server)
+manager.add_command('db',MigrateCommand)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db,Users=Users)
+
+if __name__ == '__main__':
+    manager.run()
